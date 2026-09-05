b6141df47247a36ff8e1ae378dea4ab99a184ce4
tintin210801 <tin.loncarevic@gmail.com>
2026-09-05 12:47:04 +0200

    Update data_engine.py

--- data_engine.py
+++ data_engine.py
@@ -8,7 +8,6 @@ uključujući HMM (Hidden Markov Models) filtre režima tržišta.
 import logging
 import numpy as np
 import pandas as pd
-import talib as tl
 import yfinance as yf
 from datetime import datetime, timezone, timedelta
 from hmmlearn import hmm
